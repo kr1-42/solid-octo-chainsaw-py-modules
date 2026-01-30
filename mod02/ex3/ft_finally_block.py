81b76e8595cdbaed6d06d46519b02d3120a03e42
Christian Lombardi <chrilomb@c1r6p1.42firenze.it>
2026-01-30 14:45:26 +0100

    mistake

--- mod02/ex3/ft_finally_block.py
+++ mod02/ex3/ft_finally_block.py
@@ -1,18 +1,19 @@
 def water_plants(plant_list):
-	for plant in plant_list:
-		if plant is None:
-			print("Error: can't water none - invalid plant!\n")
-			continue
-		try:
-			print(f"Watering {plant}...")
-			if plant == "cactus":
-				raise ValueError("Cacti need very little water!")
-			print(f"{plant} has been watered.\n")
-		except ValueError as e:
-			print(f"Error: {e}\n")
-		finally:
-			print(f"Finished attempt to water {plant}.\n")
+    for plant in plant_list:
+        if plant is None:
+            print("Error: can't water none - invalid plant!\n")
+            continue
+        try:
+            print(f"Watering {plant}...")
+            if plant == "cactus":
+                raise ValueError("Cacti need very little water!")
+            print(f"{plant} has been watered.\n")
+        except ValueError as e:
+            print(f"Error: {e}\n")
+        finally:
+            print(f"Finished attempt to water {plant}.\n")
+
 
 if __name__ == "__main__":
-	plants = ["rose", "tulip", None, "sunflower"]
-	water_plants(plants)
+    plants = ["rose", "tulip", None, "sunflower"]
+    water_plants(plants)
